7354984cf47b5fca67c203193fada874c14e76a9
Tomasz Mamala <tomaszmamala@gmail.com>
2022-12-16 23:47:26 +0100

    initial commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let type = "primary"
+    export let inverse = false
+</script>
+
+<button class={type} class:inverse={inverse}>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        appearance: none;
+        padding: 8px 12px;
+        font-size: 1.4rem;
+        margin-right: 50px;
+        border-radius: 4px;
+        min-width: 150px;
+        outline: none;
+    }
+
+    .primary {
+        color: #fff;
+        border: 1px solid var(--acc-500);
+        background-color: var(--acc-500);
+    }
+
+    .inverse {
+        color: var(--acc-500);
+        background-color: transparent;
+        border: 1px solid var(--acc-500);
+    }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,73 @@
+<script>
+    import Button from "./Button.svelte";
+    import { page } from '$app/stores';
+</script>
+
+<header>
+    <h1>Auction Site</h1>
+    <div class="nav">
+    <nav>
+        <a href="/auctions" class={$page.route.id == "/auctions" ? "active" : ""}>Auctions</a>
+        <a href="/auctionsILike" class={$page.route.id == "/auctionsILike" ? "active" : ""}>Auctions I like</a>
+    </nav>
+    <img src="../../graphics/Businessman 4.png" alt="photo">
+    </div>
+    <Button inverse>Logout</Button>
+</header>
+
+<style>
+    header {
+        padding: 0 50px;
+        margin: 10px auto;
+        height: 70px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        align-items: center;
+        position: relative;
+    }
+
+    header::after {
+        content: "";
+        width: 80%;
+        height: 1px;
+        background-color: #BFB7B5;
+        position: absolute;
+        bottom: 0;
+        left: 10%;
+    }
+
+    h1 {
+        display: inline-block;
+        font-size: 32px;
+        color: var(--add2-400);
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 25px;
+    }
+
+    .nav {
+        display: flex;
+        align-items: center;
+        gap: 100px;
+    }
+
+    a {
+        font-size: 24px;
+        color: var(--add2-200);
+        text-decoration: none;
+    }
+
+    .active {
+        color: var(--acc-500);
+    }
+
+    img {
+        filter : drop-shadow(0 4px 4px rgb(0, 0, 0, .25));
+    }
+
+</style>
