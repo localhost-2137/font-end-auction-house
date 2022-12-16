20e52377850cf254565ba8a0e86daff3217bb62e
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2022-12-17 00:18:28 +0100

    login page

---
+++ src/routes/auctions/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Sidebar from '../components/Sidebar.svelte';
+</script>
+
+<Sidebar />
+
+<div class="container" />
+
+<style>
+	.container {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Button from '../shared/Button.svelte';
+</script>
+
+<div class="container">
+	<div>
+		<h3>Login</h3>
+		<form>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" id="last" />
+
+			<Button>Login</Button>
+		</form>
+		<Button inverse>Register</Button>
+	</div>
+	<img src="/public/login-bg.svg" alt="" />
+</div>
+
+<style>
+	.container {
+		width: 100vw;
+		margin-top: 150px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 300px;
+		margin-bottom: 20px;
+	}
+
+	h3 {
+		text-align: center;
+		font-size: 2rem;
+		margin-bottom: 70px;
+	}
+
+	input {
+		width: 300px;
+		background: rgba(236, 179, 101, 0.25);
+		border: none;
+		border-radius: 4px;
+		font-size: 1.6rem;
+		padding: 8px;
+		margin: 10px 0;
+	}
+
+	#last {
+		margin-bottom: 50px;
+	}
+
+	img {
+		width: 500px;
+		object-fit: cover;
+		margin-left: 150px;
+	}
+</style>
